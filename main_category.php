00e3974a89023d63e82f991fd226f1f518b24781
MALRMALR <michaelalross@gmail.com>
2017-07-18 16:54:02 -0700

    catching up

--- main_category.php
+++ main_category.php
@@ -11,10 +11,11 @@
 
   <div id="kitchen-categories" class="margin-top">
     <div class="row">
-    <div class="col-md-4">
       <!-- Sidebar -->
+    <div class="col-md-3 col-sm-12">
       <div id="main-cat-sidebar" class="">
         <h3 class="cat-title"><a href="#" class="">Kitchen Categories</a></h3>
+        <div class="divider"></div>
         <ul>
           <li><a href="#">Faucets</a></li>
           <li><a href="#">Garbage Disposal</a></li>
@@ -22,14 +23,58 @@
           <li><a href="#">Sinks</a></li>
           <li><a href="#">Accessories</a></li>
         </ul>
+        <h3 class="cat-title">Product Search</h3>
+        <div class="divider"></div>
+        <form class="" action="#" method="post">
+          <input type="text" name="search" placeholder="Model # / Description">
+
+          <select class="" name="manufacturer">
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+          </select>
+          <select class="" name="Select Product Type">
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+          </select>
+
+          <select class="" name="Select Product Option">
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+          </select>
+          <select class="" name="Select Finish">
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+            <option value="">your search parameter will go here</option>
+          </select>
+          <input type="submit" value="Search">
+          <p><a href="#">Clear Search</a></p>
+        </form>
+        <!-- left rail ad -->
+        <img src="imgs/live_help_right_rail_ad.png" alt="">
+        <!-- <img src="imgs/live_help_right_rail_ad.png" alt="">
+        <img src="imgs/live_help_right_rail_ad.png" alt="">
+        <img src="imgs/live_help_right_rail_ad.png" alt="">
+        <img src="imgs/live_help_right_rail_ad.png" alt=""> -->
+
+
       </div>
     </div>
-    <div class="col-md-8 category-border">
+    <!-- end sidebar -->
+    <!-- main -->
+    <!-- kitchen faucets -->
+    <div class="col-md-9 col-sm-12 category-border">
       <!-- Main -->
       <div class="main-category">
         <!-- category images -->
         <div class="vendor-info">
-          <img class="vendor-photo" src="imgs/delta_faucet.png" alt="">
+          <img class="vendor-photo col-md-2 col-xs-1" src="imgs/delta_faucet.png" alt="">
           <img class="vendor-logo" src="imgs/delta_logo.png" alt="">
           <div class="vendor_exit">
             <a href="#"> Delta Kitchen Faucets >> </a>
@@ -44,6 +89,9 @@
             and more in beautiful long-lasting finishes and styles, like stainless steel and bronze.
           </p>
           <!-- sub categories -->
+          <div class="col-md-2">
+
+
           <ul class="sub-category">
             <li><a href="#">Pullout Faucets</a></li>
             <li><a href="#">Single Handle</a></li>
@@ -52,16 +100,318 @@
             <li><a href="#">Pot Filler</a></li>
             <li><a href="#">View All</a></li>
           </ul>
+
+          </div>
           <!-- sub category vendor photos -->
-          <img class="sub_category_photo" src="imgs/faucet_pfister.png" alt="">
-          <img class="sub_category_photo" src="imgs/faucet_grohe.png" alt="">
-          <img class="sub_category_photo" src="imgs/faucet_moen.png" alt="">
+          <div class="col-md-2 col-xs-2"><img class="sub_category_photo" src="imgs/faucet_moen.png" alt=""></div>
+          <div class="col-md-2 col-xs-2"><img class="sub_category_photo" src="imgs/faucet_grohe.png" alt=""></div>
+          <div class="col-md-2 col-xs-2"><img class="sub_category_photo" src="imgs/faucet_pfister.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+    <!-- kitchen sinks -->
+    <div class="col-md-9 col-sm-12 category-border">
+      <!-- Main -->
+      <div class="main-category">
+        <!-- category images -->
+        <div class="vendor-info">
+          <img class="vendor-photo col-md-2 col-xs-1" src="imgs/delta_faucet.png" alt="">
+          <img class="vendor-logo" src="imgs/delta_logo.png" alt="">
+          <div class="vendor_exit">
+            <a href="#"> Blanco Undermount Kitchen Sinks >> </a>
+          </div>
+        </div>
+        <!-- main category description -->
+        <div class="main-category-desc">
+          <h3><a href="#">Kitchen Sinks</a></h3>
+          <p>Experience the ultimate in faucet technology &amp; design. Offering the
+            modern homeowner the greatest selection of kitchen faucets from leading manufacturers
+            like Delta, Grohe, Pfister and Moen.  Find pullout, single handle, pot fillers, bar faucets,
+            and more in beautiful long-lasting finishes and styles, like stainless steel and bronze.
+          </p>
+          <!-- sub categories -->
+          <div class="col-md-2">
+
+
+          <ul class="sub-category">
+            <li><a href="#">Undermount</a></li>
+            <li><a href="#">Drop-In</a></li>
+            <li><a href="#">Composite</a></li>
+            <li><a href="#">Bar / Prep</a></li>
+            <li><a href="#">Farmhouse</a></li>
+            <li><a href="#">View All</a></li>
+          </ul>
+
+          </div>
+          <!-- sub category vendor photos -->
+
+          <div class="col-md-2 col-xs-2"><img class="sub_category_photo" src="imgs/faucet_moen.png" alt=""></div>
+          <div class="col-md-2 col-xs-2"><img class="sub_category_photo" src="imgs/faucet_grohe.png" alt=""></div>
+          <div class="col-md-2 col-xs-2"><img class="sub_category_photo" src="imgs/faucet_pfister.png" alt=""></div>
         </div>
       </div>
 
     </div>
+    <!-- end kitchen sinks -->
+    <!-- garbage disposal -->
+    <div class="col-md-9 col-sm-12 category-border">
+      <!-- Main -->
+      <div class="main-category">
+        <!-- category images -->
+        <div class="vendor-info">
+          <img class="vendor-photo col-md-2 col-xs-1" src="imgs/garbage_disposal.png" alt="">
+          <!-- <img class="vendor-logo" src="imgs/delta_logo.png" alt=""> -->
+          <div class="vendor_exit">
+            <!-- <a href="#"></a> -->
+          </div>
+        </div>
+        <!-- main category description -->
+        <div class="main-category-desc">
+          <h3><a href="#">Garbage Disposals</a></h3>
+          <!-- category_description -->
+          <p>Insinkerator has your drain pipes covered with their top of the line waste disposals.
+          CHoose from features like noise reduction, safety covers, extra horsepower and reversible blade
+        action to find a perfect fit.</p>
+        <div class="col-md-2 sub_cat_desc">
+          <img src="imgs/insinkerator_logo.png" alt="">
+          <h6>Grind Almost Anything.  Hear Almost Nothing</h6>
+          <p>Insinkerator is the world's largest manufacturer of food waster disposers
+          for home and commercial use.  For decades, InsinkErator has been the #1 choice
+        of professionals worldwide.</p>
+        </div>
+        <div class="col-md-2 sub_cat_desc">
+          <img src="imgs/insinkerator_septic_assist.png" alt="">
+          <h6>InSinkerator Septic Assist.</h6>
+          <p>Insinkerator is the world's largest manufacturer of food waster disposers
+          for home and commercial use.  For decades, InsinkErator has been the #1 choice
+        of professionals worldwide.</p>
+        </div>
+        <div class="col-md-2 sub_cat_desc">
+          <img src="imgs/insinkerator_evolution.png" alt="">
+          <h6>InSinkErator Evolution Excel</h6>
+          <p>Insinkerator is the world's largest manufacturer of food waster disposers
+          for home and commercial use.  For decades, InsinkErator has been the #1 choice
+        of professionals worldwide.</p>
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- hot water dispenser -->
+    <div class="col-md-9 col-sm-12 category-border">
+      <!-- Main -->
+      <div class="main-category">
+        <!-- category images -->
+        <div class="vendor-info">
+          <img class="vendor-photo col-md-2 col-xs-1" src="imgs/delta_faucet.png" alt="">
+          <img class="vendor-logo" src="imgs/insinkerator_logo_small.png" alt="">
+          <div class="vendor_exit">
+            <a href="#"> InSinkErator F-HC1100C 1100 Instant Hot Water Dispenser >> </a>
+          </div>
+        </div>
+        <!-- main category description -->
+        <div class="main-category-desc">
+          <h3><a href="#">Hot Water Dispensers</a></h3>
+          <p>Experience the ultimate in faucet technology &amp; design. Offering the
+            modern homeowner the greatest selection of kitchen faucets from leading manufacturers
+            like Delta, Grohe, Pfister and Moen.  Find pullout, single handle, pot fillers, bar faucets,
+            and more in beautiful long-lasting finishes and styles, like stainless steel and bronze.
+          </p>
+          <img src="imgs/insinkerator_logo_small.png" alt="">
+          <p>InSinkErator is the world's largest manufacturer of food waste disposers
+          for home and commercial use.  For decades, InSinkErator has been the #1 choice
+          of professionals worldwide.</p>
+          <!-- sub categories -->
+
+          <!-- sub category vendor photos -->
+
+        </div>
+      </div>
+
+    </div>
+
+    <!-- kitchen accessories -->
+    <div class="col-md-9 col-sm-12 category-border">
+      <!-- Main -->
+      <div class="main-category">
+        <!-- category images -->
+        <div class="vendor-info">
+          <img class="vendor-photo col-md-2 col-xs-1" src="imgs/dish_drain.png" alt="">
+          <!-- <img class="vendor-logo" src="imgs/delta_logo.png" alt=""> -->
+          <div class="vendor_exit">
+            <!-- <a href="#"></a> -->
+          </div>
+        </div>
+        <!-- main category description -->
+        <div class="main-category-desc">
+          <h3><a href="#">Kitchen Accessories</a></h3>
+          <!-- category_description -->
+          <p>Insinkerator has your drain pipes covered with their top of the line waste disposals.
+          CHoose from features like noise reduction, safety covers, extra horsepower and reversible blade
+        action to find a perfect fit.</p>
+        <div class="kitchen-accessories">
+          <!-- kitchen accessory sub category -->
+          <div class="col-md-2 sub_cat_desc">
+            <img src="imgs/cutting_board.png" alt="">
+            <h6><a href="">Cutting Boards</a></h6>
+          </div>
+          <!-- kitchen accessory sub category -->
+          <div class="col-md-2 sub_cat_desc">
+            <img src="imgs/air_gap.png" alt="">
+            <h6><a href="">Air Gaps</a></h6>
+
+          </div>
+          <!-- kitchen accessory sub category -->
+          <div class="col-md-2 sub_cat_desc">
+            <img src="imgs/basket_strainer.png" alt="">
+            <h6><a href="">Basket Strainers</a></h6>
+
+          </div>
+          <!-- kitchen accessory sub category -->
+          <div class="col-md-2 sub_cat_desc">
+            <img src="imgs/soap_dispenser.png" alt="">
+            <h6><a href="">Soap Dispensers</a></h6>
+
+          </div>
+
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
   </div>
+    <!-- featured products -->
+  <div class="feat-prods margin-down clearfix">
+
+		<div>
+			<h2 class="section-title">Featured Products</h2>
+			<div class="products-arrows arrows">
+	          <a class="arrow-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+	          <a class="arrow-next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+	        </div>
+		</div>
+
+		<div id="products-carousel" class="slider">
+			<div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+			<div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+	        <div class="slick-slide img-text-box">
+	          <a href="#" class="img-link"><img src="imgs/faucet.jpg"></a>
+	          <div class="desc">
+	          	<h3><a href="#">Moen Arbor 123456 Kitchen Pullout</a></h3>
+
+	          		Our Price<br>$345.78
+
+	          </div>
+	        </div>
+
+		</div>
+	</div>
+
+
+  <!-- featured collections -->
+  <div class="feat-prods margin-down clearfix">
+    <div class="">
+      <h2 class="section-title">Featured Collections</h2>
+    </div>
+    <div class="row">
+      <div class="col-sm-1 col-md-2">
+        <img src="imgs/collection_delta.png" alt="">
+      </div>
+      <div class="col-sm-1 col-md-2">
+        <img src="imgs/collection_kohler.png" alt="">
+      </div>
+      <div class="col-sm-1 col-md-2">
+        <img src="imgs/collection_blanco.png" alt="">
+      </div>
+      <div class="col-sm-1 col-md-2">
+        <img src="imgs/collection_insinkerator.png" alt="">
+      </div>
+      <div class="col-sm-1 col-md-2">
+        <img src="imgs/collection_insinkerator.png" alt="">
+      </div>
+    </div>
+  </div>
+
+
 
 </div> <!-- END container main -->
 
