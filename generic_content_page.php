a3af9bed68eaa01db4744c00ab02ab6cf9c9489c
MALRMALR <michaelalross@gmail.com>
2017-07-19 16:52:12 -0700

    generic content pager done

---
+++ generic_content_page.php
@@ -0,0 +1,38 @@
+<?php include ('header.php'); ?>
+
+<div class="container main">
+  <div id="banner" class="margin-top margin-down clearfix">
+
+      <img src="imgs/main_category_ad.png" alt="banner ad">
+
+  </div>
+
+  <div class="row">
+    <div class="col-md-12 generic-content-page">
+      <h3>About Faucet Depot.com</h3>
+      <p>Welcome to FaucetDepot.com, the Internet's premier distributor of brand name residential and commercial plumbing fixtures and accessories at the planet's lowest prices. Not only have we been doing business online since 1996, but we also have 75 years of expertise in the plumbing history under our belts to assist us in serving our valued customers. FaucetDepot.com is beloved not only by our loyal customer base in the U.S., Alaska, and Hawaii, but around the globe as well - with customers on every continent on earth. At FaucetDepot.com, our main goal is to offer the lowest possible price on the top notch plumbing products that you demand, coupled with customer service that is second to none. This means that whether you are shopping for one faucet for your guest bath or hundreds of fixtures for a hotel renovation, FaucetDepot.com is your one-stop shop for friendly and knowledgeable service and great low prices on durable, long lasting, and attractive products. </p>
+      <p class="indent"><strong>While browsing the pages of our huge inventory at FaucetDepot.com, you will find:</strong></p>
+      <ul class="indent">
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur a ex at ullamcorper. Sed lobortis quis odio non varius.</li>
+      </ul>
+    </div>
+  </div>
+  <div class="chat-online">
+    <div>Chat with an online Faucet Expert</div>
+    <p>Mon - Fri 6:00am - 4:00PM PT</p>
+  </div>
+
+  <?php include ('featured.php') ?>
+</div> <!-- END container main -->
+
+
+<?php include('footer.php'); ?>
--- page_template.php
+++ page_template.php
@@ -1,8 +1,10 @@
 <?php include ('header.php'); ?>
 
 <div class="container main">
+  <!-- sidebar & top banner ad -->
+  <?php include ('sidebar.php'); ?>
 
-
+  <?php include ('featured.php') ?>
 </div> <!-- END container main -->
 
 
