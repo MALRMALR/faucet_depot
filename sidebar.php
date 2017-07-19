94182436cf1a020e63a5500defe01b3e423c17e9
MALRMALR <michaelalross@gmail.com>
2017-07-19 00:53:30 -0700

    offloaded sidebar partial to sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,63 @@
+<div id="banner" class="row margin-top margin-down clearfix">
+
+    <img src="imgs/main_category_ad.png" alt="banner ad">
+
+</div>
+
+<div id="kitchen-categories" class="margin-top">
+  <div class="row">
+    <!-- Sidebar -->
+  <div class="col-md-3 col-sm-12">
+    <div id="main-cat-sidebar" class="">
+      <h3 class="cat-title"><a href="#" class="">Kitchen Categories</a></h3>
+      <div class="divider"></div>
+      <ul>
+        <li><a href="#">Faucets</a></li>
+        <li><a href="#">Garbage Disposal</a></li>
+        <li><a href="#">Hot Water Dispensers</a></li>
+        <li><a href="#">Sinks</a></li>
+        <li><a href="#">Accessories</a></li>
+      </ul>
+      <h3 class="cat-title">Product Search</h3>
+      <div class="divider"></div>
+      <form class="" action="#" method="post">
+        <input type="text" name="search" placeholder="Model # / Description">
+
+        <select class="" name="manufacturer">
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+        </select>
+        <select class="" name="Select Product Type">
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+        </select>
+
+        <select class="" name="Select Product Option">
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+        </select>
+        <select class="" name="Select Finish">
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+          <option value="">parameter</option>
+        </select>
+        <input type="submit" value="Search">
+        <p><a href="#">Clear Search</a></p>
+      </form>
+      <!-- left rail ad -->
+      <img src="imgs/live_help_right_rail_ad.png" alt="">
+      <!-- <img src="imgs/live_help_right_rail_ad.png" alt="">
+      <img src="imgs/live_help_right_rail_ad.png" alt="">
+      <img src="imgs/live_help_right_rail_ad.png" alt="">
+      <img src="imgs/live_help_right_rail_ad.png" alt=""> -->
+
+
+    </div>
+  </div>
