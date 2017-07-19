18b45605711244e26064fe99e9a390e2b3d31104
MALRMALR <michaelalross@gmail.com>
2017-07-19 15:07:44 -0700

    offloaded featured to partial

--- featured.php
+++ featured.php
@@ -1,9 +1,187 @@
-<?php include ('header.php'); ?>
+<!-- featured products -->
+<div class="feat-prods margin-down clearfix">
 
-<div class="container main">
+<div>
+  <h2 class="section-title">Featured Products</h2>
+  <div class="products-arrows arrows">
+        <a class="arrow-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+        <a class="arrow-next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+      </div>
+</div>
+
+<!-- begin gallery -->
+<div id="products-carousel" class="slider">
+  <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
 
 
-</div> <!-- END container main -->
+        </div>
+      </div>
+  <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
 
 
-<?php include('footer.php'); ?>
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
+
+
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
+
+
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
+
+
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
+
+
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
+
+
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
+
+
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+            <div class="featured-retail-price">Retail Price: $373.78</div>
+            <div class="featured-our-price">Our Price: $182.85</div>
+
+
+        </div>
+      </div>
+      <div class="slick-slide featured-img-textbox">
+        <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
+        <div class="featured-desc">
+          <h3><a href="#">Delta 9178-AR-DST</a></h3>
+
+            Our Price<br>$345.78
+
+        </div>
+      </div>
+
+</div>
+</div>
+
+
+<!-- featured collections -->
+<div class="feat-prods margin-down clearfix">
+<div class="">
+  <h2 class="section-title">Featured Collections</h2>
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_delta.png" alt="">
+    <div class="featured-collection-desc">
+      <a href="#">Delta Touch Faucets With Touch2o Technology</a>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_kohler.png" alt="">
+    <div class="featured-collection-desc">
+      <a href="#">Kohler Simplice Kitchen Faucets</a>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+    <div class="featured-collection-desc">
+      <a href="#">Blanco Silgranit Drop-in &amp; Undermount sinks</a>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+    <div class="featured-collection-desc">
+      <a href="#">InSinkErator Instant Hot Water Dispensers</a>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+    <div class="featured-collection-desc">
+      <a href="#">InSinkErator Garbage Disposals</a>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+    <div class="featured-collection-desc">
+      <a href="#">InSinkErator Garbage Disposals</a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="feat-prods margin-down clearfix">
+<div class="">
+  <h2 class="section-title">Popular Categories</h2>
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_delta.png" alt="">
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_kohler.png" alt="">
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <img src="imgs/collection_blanco.png" alt="">
+  </div>
+</div>
+</div>
