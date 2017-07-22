c34c64445723676042542106c26b3b3972d4e5cd
MALRMALR <michaelalross@gmail.com>
2017-07-21 17:42:10 -0700

    finished manu list page

--- manufacturer_list.php
+++ manufacturer_list.php
@@ -3,7 +3,178 @@
 <div class="container main">
   <!-- sidebar & top banner ad -->
   <?php include ('sidebar.php'); ?>
+  <div class="col-md-9 col-sm-12">
+    <div class="taxonomy">
+      <a href="#">Home </a>
+        <span> > </span>
+        <a href="#">Manufacturers </a>
+    </div>
 
+  <h3 class="cat-title">Manufacturers</h3>
+  <p>Faucet Depot caries industry renowned faucets, sink and fixture brands as well as a wide range
+  of commercial and tool brands.  Use the links to the left to narrow your selections</p>
+
+  <div id="manufacturer-list">
+      <div class="row">
+        <div class="col-md-2">
+          <div class="img-wrapper">
+
+          <img src="imgs/manu1.png" alt="">
+        </div>
+          <a href="#"><p>AB and I Foundry</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu2.png" alt="">
+          </div>
+          <a href="#"><p>Accutools</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu3.png" alt="">
+          </div>
+          <a href="#"><p>Acorn Engineering</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu4.png" alt="">
+          </div>
+          <a href="#"><p>ACT DMAND Kontrols</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu1.png" alt="">
+          </div>
+          <a href="#"><p>AB and I Foundry</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu2.png" alt="">
+          </div>
+          <a href="#"><p>Accutools</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu3.png" alt="">
+          </div>
+          <a href="#"><p>Acorn Engineering</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu4.png" alt="">
+          </div>
+          <a href="#"><p>ACT DMAND Kontrols</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+        <div class="col-md-2">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu1.png" alt="">
+          </div>
+          <a href="#"><p>AB and I Foundry</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu2.png" alt="">
+          </div>
+          <a href="#"><p>Accutools</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu3.png" alt="">
+          </div>
+          <a href="#"><p>Acorn Engineering</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu4.png" alt="">
+          </div>
+          <a href="#"><p>ACT DMAND Kontrols</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu1.png" alt="">
+          </div>
+          <a href="#"><p>AB and I Foundry</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu2.png" alt="">
+          </div>
+          <a href="#"><p>Accutools</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu3.png" alt="">
+          </div>
+          <a href="#"><p>Acorn Engineering</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu4.png" alt="">
+          </div>
+          <a href="#"><p>ACT DMAND Kontrols</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+        <div class="col-md-2 col-sm-12">
+          <div class="img-wrapper">
+            <img src="imgs/manu5.png" alt="">
+          </div>
+          <a href="#"><p>Advanced Gloves</p></a>
+        </div>
+
+      </div>
+
+  </div>
+  </div>
   <?php include ('featured.php') ?>
 </div> <!-- END container main -->
 
