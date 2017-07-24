5877030cafa3f505c7d3e565260ce16f476bd0ea
MALRMALR <michaelalross@gmail.com>
2017-07-24 15:35:49 -0700

    tweaked class system on product detail page

--- product_detail.php
+++ product_detail.php
@@ -14,22 +14,17 @@
   </div>
 
   <h3 class="cat-title">Delta 9159-AR-DST Trinsic Single Handle Pull-Down Kitchen Faucet - Stainless</h3>
-  <div class="product-slider-image">
-
+  <div class="product-slider-image col-md-5">
     <img src="imgs/kohler_parq.png" alt="">
-
     <img src="imgs/kohler_simplice.png" alt="">
-
     <img src="imgs/kohler_torq.png" alt="">
   </div>
-  <div class="product-slider-nav">
-
+  <div class="product-slider-nav col-md-5">
     <img src="imgs/kohler_parq.png" alt="">
-
     <img src="imgs/kohler_simplice.png" alt="">
-
     <img src="imgs/kohler_torq.png" alt="">
-</div>
+  </div>
+  <h3 class="cat-title">Delta 9159-AR-DST Trinsic Single Handle Pull-Down Kitchen Faucet - Stainless</h3>
 
   <?php include ('featured.php') ?>
 </div> <!-- END container main -->
