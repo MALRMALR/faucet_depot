f5bb005924643bb701c2f271048d1bec3897c702
MALRMALR <michaelalross@gmail.com>
2017-07-25 16:04:14 -0700

    begin prod cat

---
+++ product_category.php
@@ -0,0 +1,11 @@
+<?php include ('header.php'); ?>
+
+<div class="container main">
+  <!-- sidebar & top banner ad -->
+  <?php include ('sidebar.php'); ?>
+
+  <?php include ('featured.php') ?>
+</div> <!-- END container main -->
+
+
+<?php include('footer.php'); ?>
