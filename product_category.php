0b6fc867e9aa0c5291a07d8a98269ed89b4faf55
MALRMALR <michaelalross@gmail.com>
2017-07-25 16:42:59 -0700

    prod cat build out

--- product_category.php
+++ product_category.php
@@ -3,7 +3,62 @@
 <div class="container main">
   <!-- sidebar & top banner ad -->
   <?php include ('sidebar.php'); ?>
+  <div>
+      <div class="taxonomy">
+        <a href="#">Home</a>
+        <span> &#62; </span>
+        <a href="#">Manufacturers</a>
+        <span> &#62; </span>
+        <a href="#">Kohler</a>
+        <span>(1 - 12 of 391)</span>
+      </div>
+  </div>
+  <!-- grid button -->
+  <i class="fa fa-th" aria-hidden="true"></i>
+  <!-- list button -->
+  <i class="fa fa-list" aria-hidden="true"></i>
+  <!-- sort by -->
+  <!-- show -->
+  <!-- compare -->
 
+  <!-- begin grid -->
+  <div class="col-md-9">
+    <!-- individual item -->
+    <?php for($i = 0; $i < 16; ++$i) { ?>
+    <div class="prod-cat-item col-md-3">
+      <img src="imgs/kohler_faucet.png" alt="">
+      <div class="item-title-wrapper">
+        <div class="item-title">
+          Delta 9159-AR-DST Trinsic Single Handle Pull-Down Kitchen Faucet-Stainless
+        </div>
+      </div>
+      <div class="item-specs">
+        <ul>
+          <li>Model No: <strong>D9159ARDST</strong></li>
+          <li>Finish: <strong>Stainless Steel</strong></li>
+          <li>Stock: <span class="green">Available</span></li>
+          <li>Retail Price: <strong>$501.70</strong></li>
+          <li><span class="prod-cat-our-label">Our Price: </span><span class="prod-cat-our-price">$288.30</span></li>
+        </ul>
+      </div>
+      <div class="compare-item">
+        <input type="checkbox" name="Compare" value="Select"> <span>Select</span>
+        <a href="#">Compare Now</a>
+        <div class="prod-cat-stars">
+          <img src="imgs/full_star.png" alt="">
+          <img src="imgs/full_star.png" alt="">
+          <img src="imgs/full_star.png" alt="">
+          <img src="imgs/full_star.png" alt="">
+          <img src="imgs/half_star.png" alt="">
+        </div>
+        <div class="number-reviews">
+          (9 Reviews)
+        </div>
+      </div>
+    </div>
+    <!-- end individual item -->
+    <?php } ?>
+  </div>
   <?php include ('featured.php') ?>
 </div> <!-- END container main -->
 
--- product_detail.php
+++ product_detail.php
@@ -159,15 +159,17 @@
       <img src="imgs/full_star.png" alt="">
       <img src="imgs/full_star.png" alt="">
       <img src="imgs/half_star.png" alt="">
+
       <div class="overall-rating-wrapper">
-
-
-      <span class="overall-rating"><span class="numerical-rating">4.5</span> Based on 6 Reviews</span>
-      <span class="customer-recommendation">100% of Customers would recommend this product to a friend</span>
+        <span class="overall-rating"><span class="numerical-rating">4.5</span> Based on 6 Reviews</span>
+        <span class="customer-recommendation">100% of Customers would recommend this product to a friend</span>
       </div>
     </div>
+    <!-- individual reviews -->
     <div class="review">
+
       <h3 class="cat-title">Well Made Faucet</h3>
+      <!-- customer rating -->
       <div class="customer-overall-rating">
         <img src="imgs/full_star.png" alt="">
         <img src="imgs/full_star.png" alt="">
@@ -175,9 +177,11 @@
         <img src="imgs/full_star.png" alt="">
         <img src="imgs/half_star.png" alt="">
       </div>
+      <!-- review content -->
       <p>Replaced a similar Grohe faucet whose plastic parts had failed over the years *badly*. This faucet was super-easy to install thanks to the numerous youtube videos out there. I like the shape of the faucet and the feel is that of a sturdy professional faucet.</p>
     </div>
     <div class="pro customer-review col-md-4">
+      <!-- pros -->
       <h4 class="cat-title">Pros</h4>
       <ul>
         <li>Easy to Install</li>
@@ -187,9 +191,11 @@
         <li>Easy to Clean</li>
       </ul>
     </div>
+    <!-- cons -->
     <div class="con customer-review col-md-4">
       <h4 class="cat-title">Cons</h4>
     </div>
+    <!-- best uses -->
     <div class="best-uses customer-review col-md-4">
       <h4 class="cat-title">Best Uses</h4>
       <ul>
@@ -200,12 +206,11 @@
       <span>Recommend to a Friend?</span>
       <span class="green">Yes</span>
       <div class="">
-      <span>Submitted by JohnRamella in Columbus OH on 2016-04-15</span>
-      <div class="load-more-reviews">
-        <a href="#">Load More Reviews</a>
+        <span>Submitted by JohnRamella in Columbus OH on 2016-04-15</span>
+        <div class="load-more-reviews">
+          <a href="#">Load More Reviews</a>
+        </div>
       </div>
-
-    </div>
     </div>
   </div>
   </div>
