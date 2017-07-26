8e11ca5c25ebbeae993417e55f9a568341843750
MALRMALR <michaelalross@gmail.com>
2017-07-26 00:57:59 -0700

    added hover state desktop

--- featured.php
+++ featured.php
@@ -17,6 +17,10 @@
       <a href="#" class="featured-img"><img src="imgs/faucet.jpg"></a>
       <button class="add_featured" type="button" name="button"><i class="fa fa-shopping-cart" aria-hidden="true"></i> <span>Add To Cart</span></button>
       <span class="featured-sale">Sale</span>
+      <div id="feature-overlay-icon-wrapper" class="featured-overlay">
+        <i class="fa fa-plus-circle" aria-hidden="true"></i>
+        <i class="fa fa-search-plus" aria-hidden="true"></i>
+      </div>
     </div>
     <div class="featured-desc">
       <h3>
