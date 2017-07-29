329a489a85bf65c7081cb567f03dc25ff38e7599
MALRMALR <michaelalross@gmail.com>
2017-07-29 01:21:56 -0700

    finished shopping cart

---
+++ shopping-cart-page.php
@@ -0,0 +1,149 @@
+<?php include ('header.php'); ?>
+
+<div id="shopping-cart" class="container main">
+  <div class="row">
+    <div class="col-md-12 cart-header">
+      <h3 id="shopping-cart-header">Shopping Cart</h3>
+      <div class="return-from-cart">
+        Return to Previous Page
+      </div>
+      <div class="subtotal">
+        Subtotal: <span class="total-price">$594.30</span> (1 Items)
+      </div>
+    </div>
+    <div id="cart-links" class="col-md-12">
+      <div class="col-md-2 cart-link">
+        <a href="#">Continue Shopping</a>
+      </div>
+      <div class="col-md-2 cart-link">
+        <a href="#">Live Chat</a>
+      </div>
+      <div class="col-md-2 cart-link">
+        <a href="#">Save Cart</a>
+      </div>
+      <div class="col-md-2 cart-link">
+        <a href="#">Retrieve Cart</a>
+      </div>
+      <div class="col-md-4">
+        <button class="proceed-to-checkout" type="button" name="button">Proceed to Checkout</button>
+      </div>
+    </div>
+    <div class="">
+      <!-- headers -->
+      <div class="col-md-6 cart-column cart-product">
+          <div class="cart-header">Product</div>
+          <div class="cart-section">
+          <img src="imgs/faucet_grohe.png" alt="">
+            <div class="cart-product-info">
+            <ul>
+              <li><a href="product_detail.php">Grohe 23234234 Dual Spray Pull Down Kitchen Luxe</a></li>
+              <li>Faucet - <span class="customer-choice">Stainless Steel</span></li>
+              <li>Item No: <span class="customer-choice">9292334</span></li>
+              <li>Finish: <span class="customer-choice">Stainless steel</span></li>
+              <li>Brand: <span class="customer-choice">Grohe</span></li>
+              <li>Stock: <span class="customer-choice available">Available</span></li>
+              <li>Retail Price: $850.99; Our Price: $550.30</li>
+            </ul>
+
+
+            </div>
+          </div>
+      </div>
+      <div class="col-md-2 cart-column cart-quantity">
+          <div class="cart-header">
+            Quantity
+          </div>
+          <div class="cart-section">
+            <button type="button" name="button">-</button>
+            1
+            <button type="button" name="button">+</button>
+        </div>
+      </div>
+      <div class="col-md-2 cart-column cart-remove">
+          <div class="cart-header">
+            Remove
+          </div>
+          <div class="cart-section">
+            <input type="checkbox" name="" value="">
+          </div>
+      </div>
+      <div class="col-md-2 cart-column cart-price">
+          <div class="cart-header">
+            Price
+          </div>
+          <div class="cart-section">
+            <h3 class="cat-title">$383.50</h3>
+          </div>
+      </div>
+
+    </div>
+
+  </div>
+  <div class="row">
+    <div class="col-md-3 cart-copy">
+      <button type="button" name="button">Update</button>
+    </div>
+    <div class="col-md-6 cart-copy">
+      <input type="text" name="" value="">
+      <button type="button" name="button">Apply Promo Code</button>
+    </div>
+    <div class="col-md-3">
+      <h3 class="cat-title">Sub Total: $383.50</h3>
+    </div>
+    <div class="col-md-12">
+      <button class="proceed-to-checkout" type="button" name="button">Proceed to Checkout</button>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12 shipping-section">
+      <div class="">
+        <ul>
+          Shipping:
+          <li><input type="checkbox" name="" value=""> Fedex or UPS Standard Overnight</li>
+          <li><input type="checkbox" name="" value=""> 3 Day Service</li>
+          <li><input type="checkbox" name="" value=""> 2 Day Service</li>
+          <li><input type="checkbox" name="" value=""> Overnight Service</li>
+        </ul>
+
+
+
+
+
+      </div>
+    </div>
+    <div class="col-md-12 grand-total">
+      <h3 class="cat-title">Grand Total: <span>$383.50</span></h3>
+    </div>
+  </div>
+
+
+
+  <div class="row">
+    <div class="col-md-12 cart-copy">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec consequat eros, ut hendrerit sem. Suspendisse ac augue lectus. Donec sodales odio tempor ligula mattis facilisis. Ut nec lacus sed orci pulvinar cursus quis nec ipsum. Nunc dapibus velit urna, vel ornare mauris porttitor ac. Duis ac risus eget nisl consequat tincidunt. Fusce sodales vel nisl nec molestie. Cras lobortis auctor elit, sit amet consectetur tortor hendrerit eu. Nam sollicitudin venenatis elit, in ultrices ligula malesuada vel. Duis sagittis vulputate facilisis. Sed vitae bibendum est. Integer dapibus elementum facilisis. Cras tempus luctus purus eu hendrerit. Pellentesque tincidunt diam sem, quis volutpat mi finibus nec.
+    </div>
+    <div class="col-md-4 cart-copy">
+      <div class="cart-copy-header">
+        Secure Checkout
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec consequat eros, ut hendrerit sem. Suspendisse ac augue lectus. Donec sodales odio tempor ligula mattis facilisis. Ut nec lacus sed orci pulvinar cursus quis nec ipsum. Nunc dapibus velit urna, vel ornare mauris porttitor ac. Duis ac risus eget nisl consequat tincidunt. Fusce sodales vel nisl nec molestie. Cras lobortis auctor elit, sit amet consectetur tortor hendrerit eu. Nam sollicitudin venenatis elit, in ultrices ligula malesuada vel. Duis sagittis vulputate facilisis. Sed vitae bibendum est. Integer dapibus elementum facilisis. Cras tempus luctus purus eu hendrerit. Pellentesque tincidunt diam sem, quis volutpat mi finibus nec.</p>
+    </div>
+    <div class="col-md-4 cart-copy">
+      <div class="cart-copy-header">
+        Satisfaction Guaranteed
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec consequat eros, ut hendrerit sem. Suspendisse ac augue lectus. Donec sodales odio tempor ligula mattis facilisis. Ut nec lacus sed orci pulvinar cursus quis nec ipsum. Nunc dapibus velit urna, vel ornare mauris porttitor ac. Duis ac risus eget nisl consequat tincidunt. Fusce sodales vel nisl nec molestie. Cras lobortis auctor elit, sit amet consectetur tortor hendrerit eu. Nam sollicitudin venenatis elit, in ultrices ligula malesuada vel. Duis sagittis vulputate facilisis. Sed vitae bibendum est. Integer dapibus elementum facilisis. Cras tempus luctus purus eu hendrerit. Pellentesque tincidunt diam sem, quis volutpat mi finibus nec.</p>
+    </div>
+    <div class="col-md-4 cart-copy">
+      <div class="cart-copy-header">
+        Your Privacy
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec consequat eros, ut hendrerit sem. Suspendisse ac augue lectus. Donec sodales odio tempor ligula mattis facilisis. Ut nec lacus sed orci pulvinar cursus quis nec ipsum. Nunc dapibus velit urna, vel ornare mauris porttitor ac. Duis ac risus eget nisl consequat tincidunt. Fusce sodales vel nisl nec molestie. Cras lobortis auctor elit, sit amet consectetur tortor hendrerit eu. Nam sollicitudin venenatis elit, in ultrices ligula malesuada vel. Duis sagittis vulputate facilisis. Sed vitae bibendum est. Integer dapibus elementum facilisis. Cras tempus luctus purus eu hendrerit. Pellentesque tincidunt diam sem, quis volutpat mi finibus nec.</p>
+    </div>
+  </div>
+
+  <?php include ('featured.php') ?>
+</div> <!-- END container main -->
+
+
+<?php include('footer.php'); ?>
